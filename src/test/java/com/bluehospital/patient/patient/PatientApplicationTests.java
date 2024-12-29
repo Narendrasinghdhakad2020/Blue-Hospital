3fc542f693fa3e1b3e37be831aceb808a6def35e
Narendra singh Dhakad <narendrasinghdhakad2020@gmail.com>
2024-12-29 11:32:18 +0530

    Initializing the patient microservice

---
+++ src/test/java/com/bluehospital/patient/patient/PatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluehospital.patient.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
